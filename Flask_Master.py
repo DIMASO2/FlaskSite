33305e27443bc26b99ba824b5eeca8e32935458d
PC8 <eniseevanton54@gmail.com>
2024-05-05 18:12:10 +0300

    hello

--- Flask_Master.py
+++ Flask_Master.py
@@ -3,13 +3,29 @@ app = Flask(__name__)
 
 @app.route("/")
 def duplet():
-    return render_template("duplet.html")
+    main_data = {
+        "Город": "Новочебоксарск",
+        "Район": "Девятка",
+        "Контора": "Шарашкина"
+    }
+    context = {
+        "name": "Xi Jinping",
+        "age": "70",
+        "spes": "Вождь"
+    }
+    return render_template("glava.html", main_data = main_data, **context)
 
 @app.route('/contacts/')
 def contacts():
     developer_name = "Mao"
     developer_email = "mao@mail.ru"
     return render_template("agu.html", name = developer_name, email = developer_email)
+
+@app.route('/results/')
+def results():
+    data = ["python", "js", "GO", "C#", "C++", "lua", "React"]
+    return render_template("results.html", data = data)
+
 
 if __name__ == "__main__":
     app.run(debug = True)
